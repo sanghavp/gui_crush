6014ba17a6ca5054051e3e3f58341f4f2173eea2
sanghavp <hahuusang2khl@gmail.com>
2022-02-04 20:03:21 +0700

    uploade code for cruh

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Heluuu Hòa',
+    introDesc: `Cho anh hỏi em một điều này được không ???`,
+    btnIntro: 'Vâng ạ!',
+    title: 'Em tkick anh dung khonggg 😙',
+    desc: 'Nếu em không trả lời mà cố tính thoát tức là em iu anh rất nhìuuu',
+    btnYes: 'Yêu anh nhiều lắm <33',
+    btnNo: 'Cậu mơ à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Thật ra em đã thích anh từ cái nhìn đầu tiên rồi',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Sang Ha đang đợi nhận được tin nhắn của em đó!',
+    btnAccept: 'Vâng ạ <3',
+    messLink: 'https://m.me/sangtalon' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
